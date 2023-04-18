84da2e1b7bb1a716236c14da9c3b8e862a5d803f
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-04-18 23:33:35 +0300

    Add files via upload

--- Mod10HW/src/main/java/org/example/JsonStream.java
+++ Mod10HW/src/main/java/org/example/JsonStream.java
@@ -1,9 +1,10 @@
 package org.example;
 
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.regex.Matcher;
@@ -20,10 +21,10 @@ public class JsonStream {
     }
 
     static User user1 ;
-    public static String searchJson(String user){
+    public static String searchJson(String user) throws IOException, ClassNotFoundException {
         Pattern pattern = Pattern.compile("\\w{3,5}\\s\\d{2}");
         String[]textSearch = user.split( "\r\n",user.length());
-        StringBuilder json = new StringBuilder();
+        StringBuilder json = new StringBuilder(new String());
 
         for (int i = 0; i < textSearch.length; i++) {
             Matcher matcher = pattern.matcher(textSearch[i]);
@@ -32,16 +33,37 @@ public class JsonStream {
                 String[] users = textSearch[i].split("\\s",textSearch[i].length());
                 user1 = new User(users[0], users[1]);
                 Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                json.append(gson.toJson(user1)).append(",\n");
+                json.append(gson.toJson(user1));
+
             }
 
         }
+        
+//        try (PrintWriter out = new PrintWriter(new FileWriter("user.json"))) {
+//            Gson gson = new Gson();
+//            String jsonString = gson.toJson(json);
+//            out.write(jsonString);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-        return json.toString();
+        OutputStream os = new FileOutputStream("user.json");
+        ObjectOutputStream oos = new ObjectOutputStream(os);
+        oos.writeObject(json.toString());
+        oos.flush();
+        oos.close();
+
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("user.json"));
+        Object o = ois.readObject();
+
+
+        return o.toString();
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
 
         System.out.println(searchJson(user));
     }
